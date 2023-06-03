d1da19f539e3918c4fc0c96539f617238fcf66b9
Georgemounir1 <107988409+Georgemounir1@users.noreply.github.com>
2023-06-03 04:37:40 +0300

    n

---
+++ script.js
@@ -0,0 +1,14 @@
+function myResNav() {
+let x = document.getElementById("navlist2");
+  if (x.style.display === "inline-block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "inline-block";
+  }
+}
+function closeMenu() {
+  let exit= document.getElementById("navlist2");
+  if (exit.style.display === "inline-block"){
+    exit.style.display = "none";
+  }
+  }
